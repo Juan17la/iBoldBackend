bdd45b56a3e6c8e58aef91d78781fdb396d4d76e
Juan17la <juandiegolopezarias07@gmail.com>
2026-04-17 17:11:18 -0500

    Initialize Maven project with wrapper, configuration files, and basic application structure

---
+++ src/main/java/com/peciatech/ibold/IboldApplication.java
@@ -0,0 +1,13 @@
+package com.peciatech.ibold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IboldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IboldApplication.class, args);
+	}
+
+}
